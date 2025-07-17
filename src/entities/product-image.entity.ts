35d3d00159bc8ac938be204192abf7e178d4aaa9
ketamean <truongthanhtoan2003@gmail.com>
2025-07-18 01:07:11 +0700

    k

--- src/entities/product-image.entity.ts
+++ src/entities/product-image.entity.ts
@@ -21,6 +21,33 @@ const initializePrivateKey = async (): Promise<void> => {
 
     if (response.SecretString) {
       cloudFrontPrivateKey = JSON.parse(response.SecretString)["privateKey"].replace(/\\n/g, '\n');
+      cloudFrontPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAuUbVxThsrdzbzz/jteoz+vvtkuOJmDs20+V6sKfspwRwogo7
+30UQXfPX6zzRfhIBP5jDE2XjXiRPOimfnpXK3YuhMWtyoY/OkBC1W3ETN3qVAQrk
+CdQLvEyOUix9qEuwcT+i44i63uXfveTSZmH/qxJZeM6kXfepx/6e5SVM0dF6iWBZ
+v6U1WnYbZ1jGWew3VH20uFXB3nBGqeOJWNDNzFdQWgZB976VfAwlmdj0gHZ0SUlg
+6zhsacY/VbGeHByaoXpUaQXmCYIQSUADC3nDmK0JFBGMXPzYwnEqjH25+xOvBSKG
+1A2Er9YapJHM/SN/LpeRIv6SjFE/CChWfeMmIwIDAQABAoIBAHotRPCuGbS5LNZJ
+Sk7+51I96Vdw5p5Vhf1LmZF+2ub0XZXta7f64iUilKMSUw9wrhQTF6jmCn5dMUWo
+hxLbspBP8HcXZNs3ar7D1eQ797EhC7tJREFa7HY6gaucnSxq5Sp5DWBAHQK50cZ2
+a2yKeT+FdSzeD7PLitFQjdWLuFGvUWcyTyCj75OMX3dWlt4qlBP9TuPyA9BI79r4
+Kxs4+jwyuszz1iJQl25J33zUQyqRsFQVfOntUNviSjF7PPa3vl9hgm97OUqhKFwO
+CLiEti1QwWciwakSTTpH4PXAIP+daS8sAs1rYYqBW343s/Bnh3avhpZeoxu+i+6I
+S6Ew9XECgYEA9dpY6AtpJpBAFYLzO/Txrhlmm4l63zXpL8k9vZ4jOKLC+3GKavXF
+WaiGuAzuP4sBhzWm7BxmftnA3srKgy3XHeFuafKsc67+CuQ4XgKYjQdvG55dCSml
+UDV80LSFek9XjORC1zZtGIhfWZ14jTHGa5EOFtRNUALpSE3XPAfm8hkCgYEAwOxy
+VWCxmrPL4888u1MlX8Gha8MqJP6wF3bq0S011ByGJo1N0ysXQww+ZkEdMxpO68BL
+7686Z1g2xIEt+PKFN/6JBfWt2PBzef5RfVHThQTvoBAPrAJcJWdo/bRNoIFfcrEY
+KWV4SlTDIH6x4ax7KdDUWbLY5FBUEX79m/0/OZsCgYEAn156MXlsIT5y/D7V07D1
+lakLlZmqF9WHop1uM2O2azk+8eXpF2rYEkWnhtmC+5ftVquV2AqU+nfHieSlICHw
+B4OhonZYCemO7pqAW5iNMSZ3hMzEfHMNoY/6qAPNXrreAwvYtE+oqrWEMcBdbBGv
+EP2Hx465yzsH5AHpmxV4RMkCgYB78u36QKebF001zh3c1Ky8evE1BSz8m82TJcn0
+HrHo13KxLjQEKjlF0IX+uHMENDDkcqXXFqPVZe26Dqz0tww0T4rFk3TgXowyotzE
+QWA+pE84bVlEUDfXqDriVYdSSgyC6Zj2b8T7LL45EG1E6sYutYbZInkLJzj8DLTw
+FaMbZQKBgQCv0d1zPw65oDJlpT1k8jhNpkjnmDdV5fpsMxbS14K5Y+LynySi8qT+
+G5E3kPWrfagWkEEX7xY75a6xZgtIK8D/GXaCddKlqGlb4FnY/X0/rd9YTbN1eWfo
+NpPmIIebV/ntH7k7wts47VQ7q+tQ7EReHXHgDqEi5COkcGgYxgiw6w==
+-----END RSA PRIVATE KEY-----`
       console.log('Successfully fetched and cached CloudFront private key.');
     } else {
       throw new Error('SecretString is empty.');
